796bf47cb69273eef9e0137015572c0a24c7f888
parfaitebobissepro <parfaitebobissepro@gmail.com>
2023-04-08 20:14:02 +0200

    clone of business page #done

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,27 @@ $(window).scroll(function() {
         $("header[role='banner']").removeClass('sticky');
     }
 });
+
+
+
+
+// A $( document ).ready() block.
+$(document).ready(function() {
+    // get the target div element
+    const targetClipPath = $('#mask_1 path');
+
+    // function to check screen width and add show class if needed
+    function checkScreenWidth() {
+        if (window.matchMedia("(min-width: 479.95px)").matches) {
+            targetClipPath.attr('d', 'M0,100 C30,40 40,80 80,0 L100,0 100,100');
+        } else {
+            targetClipPath.attr('d', 'M0,100 C30,56 30,100 100,27 L100,100');
+        }
+    }
+
+    // check screen width on page load
+    checkScreenWidth();
+
+    // add event listener for screen resize
+    window.addEventListener('resize', checkScreenWidth);
+});
